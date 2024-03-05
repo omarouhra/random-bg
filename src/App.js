73971d83e829e21bff979bf735d2622385337c6d
Omar Ouhra <omarmaroc26@gmail.com>
2024-03-06 00:35:52 +0100

    feat: solution v1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 function App() {
-  return <div>Amigoscode React App Template</div>;
+  const [index, setIndex] = useState(0);
+  const colors = ["red", "green", "yellow", "purple", "black"];
+
+  const getRandomColor = () => {
+    let randomNumber = Math.floor(Math.random() * colors.length);
+    setIndex(randomNumber);
+  };
+
+  return (
+    <div style={{ backgroundColor: colors[index], height: "100vh" }}>
+      <button onClick={() => getRandomColor()}>Change color {index}</button>
+    </div>
+  );
 }
 
 export default App;
